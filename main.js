81d51927fe5c3b1db4590d1d1b66da10f08e074c
mapincelli <marcelopincellit@hotmail.com>
2023-06-05 15:58:19 -0300

    inicial

---
+++ main.js
@@ -0,0 +1,13 @@
+const { app, BrowserWindow } = require('electron') ;
+function createwindow (){
+        const win = new BrowserWindow({
+            width: 1280,
+            height: 600
+        });
+    win.loadFile('src/index.html');
+}
+
+app.whenReady().then(createwindow);
+app.on("window-all-closed", () => {
+    if(process.platform !== 'darwin') app.quit()
+});
